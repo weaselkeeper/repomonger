7c3eff68478d530d9f0267f7c587ff61da467410
Jim Richardson <weaselkeeper@gmail.com>
2013-08-25 12:12:34 -0700

    add config file cli option, add a bit of error checking, and improve logging

--- src/repomonger.py
+++ src/repomonger.py
@@ -10,6 +10,7 @@ email: weaselkeeper@gmail.com
 """
 import os
 import sys
+from ConfigParser import SafeConfigParser
 import logging
 try:
     from pymongo import Connection
@@ -26,16 +27,31 @@ console.setLevel(logging.WARN)
 logging.getLogger("usefulidiot").addHandler(console)
 log = logging.getLogger("usefulidiot")
 
+### Set some variables and constants.
+CONFIGFILE = '/etc/repomonger/repomonger.conf'
 
-def config():
+
+def get_config(args,CONFIGFILE):
     # Now parse the config file.  Get any and all info from config file.
-    from ConfigParser import SafeConfigParser
     parser = SafeConfigParser()
-    if os.path.isfile('/etc/repomonger.conf'):
-        config = '/etc/repomonger.conf'
+    configuration = {}
+    if os.path.isfile(CONFIGFILE):
+        config = CONFIGFILE
     else:
-        config = '/etc/repomonger.conf'
-    log.warn('building repo %s' % config.repo)
+        log.warn('No config file found at %s' % CONFIGFILE)
+        sys.exit(1)
+    try:
+        if args.repo:
+            repo = args.repo
+        else:
+            repo = parser.get('Repomonger','repo')
+    except:
+        log.warn('config parse failed')
+        sys.exit(1)
+    log.warn('building repo %s' % repo)
+    parser.read(config)
+    return parser
+
 
 
 # Here we start if called directly (the usual case.)
@@ -44,20 +60,32 @@ if __name__ == "__main__":
 
     import argparse
 
-    cmd_parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description='Time to build a yum repo')
-    cmd_parser.add_argument('-n', '--dry-run', dest='dryrun',
+    parser.add_argument('-n', '--dry-run', dest='dryrun',
         action='store_true', help='Dry run, do not actually perform action',
         default=False)
-    cmd_parser.add_argument('-d', '--debug', dest='debug',
+    parser.add_argument('-d', '--debug', dest='debug',
         action='store_true', help='Enable debugging during execution.',
         default=None)
-    cmd_parser.add_argument('-r', '--readable', dest='human_readable',
+    parser.add_argument('-r', '--readable', dest='human_readable',
         action='store_true', default=False,
         help='Display output in human readable formant (as opposed to json).')
-    cmd_parser.add_argument('-c', '--config', dest='config_override',
+    parser.add_argument('-c', '--config',
         action='store', default=None,
         help='Specify a path to an alternate config file')
-    cmd_parser.add_argument('-R', '--repo', dest='reponame',
-        action='store', default=None,
+    parser.add_argument('-R', '--repo', action='store', default=None,
         help='Name of repo to build. See definition in config')
+
+    args = parser.parse_args()
+    args.usage = "check_unmonitored.py [options]"
+
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.WARN)
+    if args.config:
+        CONFIGFILE = args.config
+
+    _parse_config = get_config(args,CONFIGFILE)
